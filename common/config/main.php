c56e8cb95c5a73e404b5b92015dbe0a45029c8a6
king199025 <korol_dima@list.ru>
2016-03-14 12:53:08 +0300

    first

---
+++ common/config/main.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+    'modules' => [
+        'rbac' => [
+            'class' => 'dektrium\rbac\Module',
+        ],
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableUnconfirmedLogin' => true,
+            'enableGeneratingPassword' => true,
+            'enableConfirmation' => true,
+            'enableFlashMessages' => false,
+            'confirmWithin' => 86400,
+            'cost' => 12,
+            'admins' => ['admin'],
+            'mailer' => [
+                'sender'                => 'admin@carbax.ru', // or ['no-reply@myhost.com' => 'Sender name']
+                'welcomeSubject'        => 'Добро пожаловать',
+                'confirmationSubject'   => 'Confirmation subject',
+                'reconfirmationSubject' => 'Email change subject',
+                'recoverySubject'       => 'Recovery subject',
+            ],
+        ],
+    ],
+];
---
+++ frontend/config/main.php
@@ -0,0 +1,48 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'language' => 'ru-RU',
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'request' => [
+            'baseUrl' => '',
+        ],
+        /*'user' => [
+            'identityClass' => 'dektrium\user\Module',
+            'enableAutoLogin' => true,
+        ],*/
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => false,
+        ],
+    ],
+    'params' => $params,
+];
