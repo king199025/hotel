1f0fd2f364c2bc2904caab426520f0e1dd6d0d0b
AKD47 <van83horn@gmail.com>
2016-04-28 14:26:44 +0300

    date position fix

--- frontend/modules/news/views/default/index.php
+++ frontend/modules/news/views/default/index.php
@@ -17,7 +17,7 @@ $this->title = Yii::t('title','TITLE_NEWS');
     <div class="row">
         <?php $i=1; foreach($news as $n): ?>
             <?php if($i == 2 || $i == 9): ?>
-                <div class="col-lg-6 col-sm-6 col-xs-12" style="margin: 20px auto">
+                <div class="col-lg-6 col-sm-6 col-xs-12" style="margin: 10px auto">
 
                     <div class="content__press--center">
                         <img class="content__press--center__img" src="<?= $n->images; ?>" alt=""/>
@@ -33,7 +33,7 @@ $this->title = Yii::t('title','TITLE_NEWS');
                 </div>
             <?php else: ?>
                 <?php if(empty($n->images)): ?>
-                    <div class="col-lg-3 col-sm-3 col-xs-12" style="margin: 20px auto">
+                    <div class="col-lg-3 col-sm-3 col-xs-12" style="margin: 10px auto">
                         <div class="content__press--right">
 
                                 <h3><?= $n->title;?></h3>
@@ -44,7 +44,7 @@ $this->title = Yii::t('title','TITLE_NEWS');
                         </div>
                     </div>
                 <?php else: ?>
-                    <div class="col-lg-3 col-sm-3 col-xs-12" style="margin: 20px auto">
+                    <div class="col-lg-3 col-sm-3 col-xs-12" style="margin: 10px auto">
                         <div class="content__press--left">
                             <div class="content__press--left__img">
                                 <img src="<?= $n->images; ?>" alt="News">
@@ -53,7 +53,7 @@ $this->title = Yii::t('title','TITLE_NEWS');
                             <p><?= $n->title;?></p>
                             <a class="content__press--link" href="<?= Url::to(['view','id'=>$n->id]); ?>"><?= Yii::t('button', 'BUTTON_READ');?> &rarr;</a>
 
-                            <p class="content__press--date"><?= date('d-m-Y', $n->dt_add); ?></p>
+                            <p class="content__press--date3"><?= date('d-m-Y', $n->dt_add); ?></p>
 
                         </div>
                     </div>
